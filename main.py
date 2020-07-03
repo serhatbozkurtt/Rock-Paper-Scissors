4d79304d2a2a99c01982e702b5cc6a7a3678b49a
serhatbozkurtt <65898491+serhatbozkurtt@github.com>
2020-07-04 01:04:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 root = Tk()
 root.title('Learn To Code at Codemy')
-# root.geometry("1000x1000")
+root.geometry("600x300")
 root.resizable(width=False, height=False)
 
 
@@ -16,7 +16,8 @@ image_list = [rock, paper, scissors]
 
 
 myLabel1 = Label(root, text=image_list)
-
+mylabel44 = Label(root, text="")
+mylabel44.grid(row=0, column=1)
 
 def mix1():
     global myLabel1
@@ -38,13 +39,14 @@ def mix2():
     myLabel2.destroy()
     #myLabel2 = Label(root, image=image_list[0])
     myLabel2 = Label(root, image=user_choice)
-    myLabel2.grid(row=0, column=1)
+    myLabel2.grid(row=0, column=2)
     mix1()
     #text = image_list[0]
 
     winwin(user_choice)
 
 w_label = Label(root, text="")
+
 
 def winwin(text):
     global w_label
@@ -53,7 +55,7 @@ def winwin(text):
 
 
     w_label.grid_forget()
-    # w_label.destroy()
+    w_label.destroy()
 
     # print(oyuncu1)
     # print(oyuncu2)
@@ -62,38 +64,46 @@ def winwin(text):
     # print(image_list[2])
 
     if oyuncu1 == oyuncu2:
-        w_label = Label(root, text="Berabere")
+        w_label = Label(root, text="Draw")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[0] and oyuncu2 == image_list[2]:
-        w_label = Label(root, text="1. oyuncu kazandı")
+        w_label = Label(root, text="Player 1 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[0] and oyuncu2 == image_list[1]:
-        w_label = Label(root, text="2. oyuncu kazandı")
+        w_label = Label(root, text="Player 2 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[2] and oyuncu2 == image_list[0]:
-        w_label = Label(root, text="2. oyuncu kazandı")
+        w_label = Label(root, text="Player 2 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[1] and oyuncu2 == image_list[2]:
-        w_label = Label(root, text="2. oyuncu kazandı")
+        w_label = Label(root, text="Player 2 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[1] and oyuncu2 == image_list[0]:
-        w_label = Label(root, text="1. oyuncu kazandı")
+        w_label = Label(root, text="Player 1 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     elif oyuncu1 == image_list[2] and oyuncu2 == image_list[1]:
-        w_label = Label(root, text="1. oyuncu kazandı")
+        w_label = Label(root, text="Player 1 win")
         w_label.grid(row=4, column=1)
+        w_label.place(x=250, y=250)
     else:
-        print("oyun bitti")
+        print("Game Over")
+
 
 myLabel2 = Label(root, text="")
 myLabel2.grid(row=0, column=1)
 
-# mybutton1 = Button(root, text="1. yi Değiştir", command=mix1)
-# mybutton1.grid(row=3, column=0)
-
-mybutton2 = Button(root, text="OYNA", command=mix2)
-mybutton2.grid(row=3, column=0)
 
 
+mybutton2 = Button(root, text="PLAY", command=mix2)
+mybutton2.grid(row=3, column=1 )
+
+
+mybutton2.place(x=250, y=225)
 
 root.mainloop()
